fb40b8e3b090888021e281335c88ab63f0596f04
Daniel D’Aquino <daniel@daquino.me>
2023-11-15 20:25:32 +0000

    Improve draft

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,11 @@
-# Use Alpine Linux as the base image
-FROM alpine:latest
-
-# Install necessary build tools
-RUN apk --update add \
-    build-base \
-    && rm -rf /var/cache/apk/*
+FROM neomantra/flatbuffers
 
 ENV PATH="/usr/local/bin:${PATH}"
 
-# Copy Flatbuffers binaries and libraries from neomantra/flatbuffers image
-COPY --from=neomantra/flatbuffers /usr/local/bin/flatc /usr/local/bin/flatc
-COPY --from=neomantra/flatbuffers /usr/local/include/flatbuffers /usr/local/include/flatbuffers
-COPY --from=neomantra/flatbuffers /usr/local/lib/libflatbuffers.a /usr/local/lib/libflatbuffers.a
-COPY --from=neomantra/flatbuffers /usr/local/lib/cmake/flatbuffers /usr/local/lib/cmake/flatbuffers
-
-# Copy Flatcc binaries and libraries from neomantra/flatbuffers image
-COPY --from=neomantra/flatbuffers /usr/local/bin/flatcc /usr/local/bin/flatcc
-COPY --from=neomantra/flatbuffers /usr/local/include/flatcc /usr/local/include/flatcc
-COPY --from=neomantra/flatbuffers /usr/local/lib/libflatcc.a /usr/local/lib/libflatccrt.a /usr/local/lib/
-
-COPY --from=neomantra/flatbuffers /usr/local /usr/local
-
-# Add execution permission for the above flatc and flatcc binaries
-RUN chmod +x /usr/local/bin/flatc /usr/local/bin/flatcc
-
-# Check if `/usr/local/bin/flatcc` exists in the image and print its version
-RUN /usr/local/bin/flatcc --version
-
-# Add permissive permissions to /usr/local
-RUN chmod -R 777 /usr/local
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    git \
+    gcc
 
 # Copy build.sh, eclectic.fbs, and myissue.c from the host into an /app directory in the image
 COPY build.sh eclectic.fbs myissue.c /app/
--- myissue.c
+++ myissue.c
@@ -17,29 +17,41 @@ int main(int argc, char *argv[])
 
     char* json = "{\"name\":\"ひらがな\",\"height\":2}";
     
-    /* Parse the JSON into a FlatBuffers table. */
-    flatbuffers_parser_t parser;
-    flatbuffers_parser_init(&parser);
-    if (!flatbuffers_parser_parse(&parser, json, strlen(json), 1)) {
-        printf("could not parse JSON: %s\n", flatbuffers_parser_error_string(&parser));
-        return 1;
-    }
-    flatcc_json_parser_t ctx;
-    flatcc_json_parser_init(&ctx, flatbuffers_parser_get_buf(&parser), flatbuffers_parser_get_buf_size(&parser));
-    flatcc_json_parser_skip_whitespace(&ctx);
-    flatbuffers_table_t ref;
-    buf = Eclectic_FooBar_parse_json_table(&ctx, buf, end, &ref);
-    if (!buf) {
-        printf("could not parse JSON as FlatBuffers table: %s\n", flatcc_json_parser_error_string(&ctx));
+    // Parse JSON into a flatbuffer
+    Eclectic_FooBar_ref_t ref;
+    if (!parse_json(B, json, strlen(json), 0, &ref)) {
+        printf("failed to parse json\n");
         return 1;
     }
     
-    // Check and print the "name" field of the parsed table.
-    if (!Eclectic_FooBar_name_has_value(ref)) {
-        printf("name field is missing\n");
-        return 1;
+    // Check and print the "name" field of the flatbuffer
+    if (Eclectic_FooBar_name_is_present(ref)) {
+        printf("name: %s\n", Eclectic_FooBar_name(ref));
+    } else {
+        printf("name is not present\n");
     }
 
     flatcc_builder_clear(B);
     return ret;
+}
+
+static int parse_json(flatcc_builder_t *B,
+        const char *buf, size_t bufsiz, int flags, Eclectic_FooBar_ref_t *profile)
+{
+	flatcc_json_parser_t parser, *ctx = &parser;
+	flatcc_json_parser_init(ctx, B, buf, buf + bufsiz, flags);
+
+	if (flatcc_builder_start_buffer(B, 0, 0, 0))
+		return 0;
+
+	Eclectic_FooBar_parse_json_table(ctx, buf, buf + bufsiz, profile);
+	if (ctx->error)
+		return 0;
+ 
+	if (!flatcc_builder_end_buffer(B, *profile))
+		return 0;
+
+	ctx->end_loc = buf;
+
+	return 1;
 }
